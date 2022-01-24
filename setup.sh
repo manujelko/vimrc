7e873ab8d0358a0fe5dc9e5beafebd59494e0413
Manuel Šarić <manuel.saric@2iqresearch.com>
2022-01-24 16:41:20 +0000

    Make setup.sh work on mac

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s `readlink -f init.vim` ~/.vimrc
-ln -s `readlink -f vim` ~/.vim
+ln -s `pwd`/init.vim ~/.vimrc
+ln -s `pwd`/vim/ ~/.vim
 git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
 
